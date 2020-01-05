c4ece4a67d493c012fad5d2cd7e484ba4c7098b0
Javad Shafique <javadshafique@hotmail.com>
2020-01-05 21:02:02 +0100

    Update steam.py

--- steam/steam.py
+++ steam/steam.py
@@ -70,10 +70,10 @@ class steam:
                 if result[kn] != "None":
                     response += "**{}**: {}\n".format(kn.upper(), result[kn])
             
-            await ctx.bot.send_message(response)
+            await ctx.bot.send_message(ctx.message.channel, response)
 
         else:
-            await ctx.bot.send_message("> Invalid input.")
+            await ctx.bot.send_message(ctx.message.channel, "> Invalid input.")
 
 def setup(bot):
     bot.add_cog(steam(bot))  
