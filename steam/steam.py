4594fbd2dec076577e82a40ab39494968f5960b9
Javad Shafique <javadshafique@hotmail.com>
2020-01-11 02:38:22 +0100

    adad

--- steam/steam.py
+++ steam/steam.py
@@ -14,7 +14,7 @@ BAITED_SERVERS = {
     "meta":{
         "eu":":flag_eu: #{} Baited 5v5 Competitive !knife !ws !gloves ",
         "na":":flag_us: #{} Baited 5v5 Competitive !knife !ws !gloves ",
-        "ip":"[{}]({})"
+        "ip":"[{}](steam://connect/{})"
     },
     "eu":[
         "145.239.254.11:27015",
